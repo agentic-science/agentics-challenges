8a25ca43e387c0b31f9dd7df93c7ba8a527eeeb3
ifsheldon <feng.liang@kaust.edu.sa>
2026-05-27 14:40:10 +0800

    feat(challenges): add cant-late-ha-tight-large-frontier-cs-cbl-ha-tl
    Port Frontier-CS `research/problems/cant_be_late/high_availability_tight_deadline_large_overhead` as `cant-late-ha-tight-large-frontier-cs-cbl-ha-tl`.

---
+++ test-solutions/cant-late-ha-tight-large-frontier-cs-cbl-ha-tl/solution.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+import argparse
+from sky_spot.strategies.strategy import Strategy
+from sky_spot.utils import ClusterType
+class Solution(Strategy):
+    NAME="agentics_smoke_on_demand"
+    def solve(self,spec_path:str): return self
+    def _step(self,last_cluster_type,has_spot): return ClusterType.ON_DEMAND
+    @classmethod
+    def _from_args(cls,parser:argparse.ArgumentParser):
+        args,_=parser.parse_known_args(); return cls(args)
